358b7fcc311e3ab770c60c3c722550c34c55f7f2
MaryPiva <mari.devpro@gmail.com>
2025-01-23 14:59:55 +0000

    Update Program.cs

--- Back/src/Proeventos.API/Program.cs
+++ Back/src/Proeventos.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Proeventos.API
+namespace ProEventos.API
 {
     public class Program
     {
