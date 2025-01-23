092f021327c4a5cfb95755878a93a74d5627b81d
MaryPiva <mari.devpro@gmail.com>
2025-01-23 14:58:27 +0000

    Update WeatherForecastController.cs

--- Back/src/Proeventos.API/Controllers/WeatherForecastController.cs
+++ Back/src/Proeventos.API/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Proeventos.API.Controllers
+namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
