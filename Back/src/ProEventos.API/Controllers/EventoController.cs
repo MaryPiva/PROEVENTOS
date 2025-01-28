a4e5eb4798ed6fe0a529ae42c3b31afb68ade3be
MaryPiva <mari.devpro@gmail.com>
2025-01-28 16:35:55 +0000

    Update EventoController.cs

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -18,9 +19,29 @@ namespace ProEventos.API.Controllers
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-           return "value";
+           return new Evento[]{
+                new Evento(){  
+                    EventoId = 1,
+                    Tema = "Angular 11 e .net 5",
+                    Local = "Londres",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+               },
+                new Evento(){  
+                    EventoId = 1,
+                    Tema = "Angular 11 e .net 5",
+                    Local = "Londres",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                }
+          };
+           
         }
     }
 }
