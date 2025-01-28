ea43f9caa3c9ec66598bec59074af4b8aeb9e026
MaryPiva <mari.devpro@gmail.com>
2025-01-28 16:59:02 +0000

    Update EventoController.cs

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -12,36 +12,45 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-       
+        public IEnumerable<Evento> _evento = new Evento[]{  
+                new Evento(){  
+                    EventoId = 1,
+                    Tema = "Angular 11 e .net 5",
+                    Local = "Londres",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+               },
+                new Evento(){  
+                    EventoId = 2,
+                    Tema = "Angular 11 e suas novidades",
+                    Local = "Brazil",
+                    Lote = "2 Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto1.png"
+                }
+          };
 
         public EventoController()
         {   
         }
 
         [HttpGet]
-        public IEnumerable<Evento> Get()
-        {
-           return new Evento[]{
-                new Evento(){  
-                    EventoId = 1,
-                    Tema = "Angular 11 e .net 5",
-                    Local = "Londres",
-                    Lote = "1 Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemURL = "foto.png"
-               },
-                new Evento(){  
-                    EventoId = 1,
-                    Tema = "Angular 11 e .net 5",
-                    Local = "Londres",
-                    Lote = "1 Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemURL = "foto.png"
-                }
-          };
-           
+        public IEnumerable<Evento> get(){
+            return _evento;
+        }
+
+        [HttpGet ("{id}")]
+        public IEnumerable<Evento> getById(int id){
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost ]  
+
+        public string Post(){
+            return "Exemplo Post";
         }
     }
 }
