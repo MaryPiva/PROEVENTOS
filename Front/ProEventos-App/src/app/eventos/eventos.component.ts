a756b5b9a3101f281f725f379f52fdbccdab0fc9
MaryPiva <mari.devpro@gmail.com>
2025-02-05 23:07:36 +0000

    Update eventos.component.ts

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any = [
+    {
+      tema:'Angular 11',
+      local: 'Belo Horizonte'
+    },
+    {
+      tema:'.NET 5',
+      local: 'Sao Paulo'
+    },
+    {
+      tema:'Angular e suas novidades',
+      local: 'Rio de Janeiro'
+    }
+]
+
   constructor() { }
 
   ngOnInit() {
