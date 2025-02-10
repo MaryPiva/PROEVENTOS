f2f355633dfb144a3fd7d5bf758c1c1a349a4f55
MaryPiva <mari.devpro@gmail.com>
2025-02-10 22:57:04 +0000

    Update main.ts

--- Front/ProEventos-App/src/main.ts
+++ Front/ProEventos-App/src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';  // Importando 'withFetch'
+import { provideHttpClient, withFetch } from '@angular/common/http';  
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(withFetch())  // Configura 'fetch' para o HttpClient
+    provideHttpClient(withFetch())
   ]
 })
   .catch(err => console.error(err));
