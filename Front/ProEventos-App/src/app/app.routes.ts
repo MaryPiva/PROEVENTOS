f746272f0702b5ff3100b2fb877f527bfb1144c1
MaryPiva <mari.devpro@gmail.com>
2025-02-15 20:50:58 +0000

    Update app.routes.ts

--- Front/ProEventos-App/src/app/app.routes.ts
+++ Front/ProEventos-App/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EventosComponent } from './eventos/eventos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'eventos', component: EventosComponent }
+];
