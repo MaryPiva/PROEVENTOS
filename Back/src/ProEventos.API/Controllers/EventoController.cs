3578c3a86ef9c78fd2460c4878752fb5249ccb69
MaryPiva <mari.devpro@gmail.com>
2025-02-28 20:33:30 +0000

    Update EventoController.cs

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -20,7 +20,28 @@ namespace ProEventos.API.Controllers
         [HttpGet]
         public string Get()
         {
-            return "value";
+            return "Exemplo de get";
         }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id={id}";
+        }
+
+         [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id={id}";
+        }
+
+     
     }
 }
